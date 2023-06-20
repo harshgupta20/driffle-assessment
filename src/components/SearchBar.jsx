86fae26e296d76d9d51308ec7911681d039a468f
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-06-21 00:28:51 +0530

    searchbar completd

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "../styles/SearchBar.css";
+
+// SEARCH IMAGE
+import SearchImage from "../assets/search-icon.jpg";
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <>
+    <div id="search-main">
+      <img id="search-img" src={SearchImage} alt="searchIcon"/>
+      <input id="searchbar" type="text" placeholder='Search...' />
+    </div>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../styles/Home.css";
 
 // COMPONENTS
 import Navbar from "../components/Navbar";
