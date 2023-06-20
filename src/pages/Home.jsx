64f41267d184533e73695639358cb86881e34552
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-06-21 00:36:44 +0530

    theme changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,21 @@ import "../styles/Home.css";
 // COMPONENTS
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
 
 const Home = () => {
   return (
     <>
-        <Navbar/>
-        <div id="home-main">
-            <div id="home-searchBar">
-              <SearchBar/>
-            </div>
+      <Navbar />
+      <div id="home-main">
+        <div id="home-searchBar">
+          <SearchBar />
         </div>
+
+        <div id="cards-main">
+          <Card/>
+        </div>
+      </div>
     </>
   )
 }
