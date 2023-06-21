a6c835226370c08b13cf9db93f1aabd15e39954f
harshgupta20 <hgreatgupta2001@gmail.com>
2023-06-21 12:24:05 +0530

    Add modal completed

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,40 +1,30 @@
-import { Box, Button, Modal, Typography } from '@mui/material'
-import { useState } from 'react'
+import { Modal } from '@mui/material'
+import "../styles/AddModal.css";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-const AddModal = () => {
+// DRIFFLE LOGO
+import LOGO from "../assets/driffle-logo.png";
 
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
+const AddModal = ({ open, setOpen }) => {
+
   const handleClose = () => setOpen(false);
+
   return (
     <>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <div id="add-modal-main">
+          <div id="add-modal-data">
+            <h3 id="add-modal-heading"><img src={LOGO} style={{ width: "30px", backgroundColor: "#212121", marginRight: "20px" }} alt="" />Add Note</h3>
+            <input type="text" name="title" placeholder='Title...' id="add-modal-title" />
+            <textarea name="" cols="30" rows="10" id="add-modal-textarea" placeholder='Desciption...'></textarea>
+            <button id="add-modal-btn">+ Add Note</button>
+          </div>
+        </div>
       </Modal>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/Navbar.css";
 
 // DRIFFLE WEBSITE LOGO
 import Logo from "../assets/driffle-logo.svg";
+import AddModal from './AddModal';
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+
   return (
     <>
         <nav id="nav-main">
             <img id="nav-logo" src={Logo} alt="logo" />
-            <button id="nav-btn-add">+ Add Note</button>
+            <button onClick={handleOpen} id="nav-btn-add">+ Add Note</button>
         </nav>
+        <AddModal open={open} setOpen={setOpen}/>
     </>
   )
 }
