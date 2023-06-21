8ce476800ac6190d8d8faeccbd0f78be85725a93
harshgupta20 <hgreatgupta2001@gmail.com>
2023-06-21 12:33:40 +0530

    Add note functionality is completed

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -9,6 +9,28 @@ const AddModal = ({ open, setOpen }) => {
 
   const handleClose = () => setOpen(false);
 
+  const AddDataInStorage = () => {
+    const title = document.getElementById('add-modal-title').value;
+    const desc = document.getElementById('add-modal-textarea').value;
+    const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString();
+    const id = new Date().getTime();
+
+    if (title && desc) {
+      const data = { title, desc, date, time, id }
+      const oldData = JSON.parse(localStorage.getItem('data'));
+      if (oldData) {
+        const newData = [...oldData, data];
+        localStorage.setItem('data', JSON.stringify(newData));
+      } else {
+        localStorage.setItem('data', JSON.stringify([data]));
+      }
+      handleClose();
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
   return (
     <>
       <Modal
@@ -22,7 +44,7 @@ const AddModal = ({ open, setOpen }) => {
             <h3 id="add-modal-heading"><img src={LOGO} style={{ width: "30px", backgroundColor: "#212121", marginRight: "20px" }} alt="" />Add Note</h3>
             <input type="text" name="title" placeholder='Title...' id="add-modal-title" />
             <textarea name="" cols="30" rows="10" id="add-modal-textarea" placeholder='Desciption...'></textarea>
-            <button id="add-modal-btn">+ Add Note</button>
+            <button onClick={AddDataInStorage} id="add-modal-btn">+ Add Note</button>
           </div>
         </div>
       </Modal>
